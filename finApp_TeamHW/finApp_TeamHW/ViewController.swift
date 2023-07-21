7e186435df004687d218b4660a8bec634eae62b9
fattony <a.kondratenko.dev@gmail.com>
2023-07-21 23:55:46 +0200

    add project

---
+++ finApp_TeamHW/finApp_TeamHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  finApp_TeamHW
+//
+//  Created by Anton Kondratenko on 22.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
